cb3f60326c6a142ee56f3a8e4d340ee06c753436
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-11 22:58:54 +0530

    printing a sequence

---
+++ Loops/PrintSequence.java
@@ -0,0 +1,15 @@
+package Loops;
+import java.util.Scanner;
+public class PrintSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No of tern (n): ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+            System.out.println(n);
+            n -= 1;
+        }
+    }
+}
