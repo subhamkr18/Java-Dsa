60e4c809efaff1b20bf882ef60387530092ef3af
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-12 00:10:31 +0530

    Do whileloop

---
+++ Loops/DO_While.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class DO_While {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.print(i +" ");
+            i++;
+        }while(i<=5);
+    }
+}
