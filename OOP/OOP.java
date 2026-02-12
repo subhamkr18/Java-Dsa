735d33ad586bea1becf490ffaed3d1d33e3faa00
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-12 23:35:16 +0530

    Learning Basic how object created and called

--- OOP/OOP.java
+++ OOP/OOP.java
@@ -7,6 +7,20 @@ class Pen{
     public void write(){
         System.out.println("Write Something");
     }
+
+    public void printColour(){
+        System.out.println(this.colour);
+    }
+}
+
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println("Name: "+this.name);
+        System.out.println("age: "+this.age);
+    }
 }
 
 public class OOP {
@@ -14,7 +28,19 @@ public class OOP {
         Pen pen1 = new Pen();
         pen1.colour = "Blue";
         pen1.type= "Gel";
+
+        Pen pen2 = new Pen();
+        pen2.colour = "Black";
+        pen2.type = "ballpoint";
+
+        pen1.printColour();
+        pen2.printColour();
         pen1.write();
+
+        Student s1 = new Student();
+        s1.name = "Subham";
+        s1.age = 20;
+        s1.printInfo();
     }
     
 }
