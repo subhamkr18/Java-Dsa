4f85180be5e7a8c2ed5b3bbcd69625d4ce99b3a2
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-13 22:32:00 +0530

    Printing a pattern as per user input

---
+++ Pattern_Printing/StarRec_Input.java
@@ -0,0 +1,18 @@
+package Pattern_Printing;
+import java.util.Scanner;
+
+public class StarRec_Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row: ");
+        int row = sc.nextInt();
+        System.out.print("Enter Col: ");
+        int col = sc.nextInt();
+
+        for (int i=1; i<=row; i++){
+            for(int j=1; j<=col;j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+}
