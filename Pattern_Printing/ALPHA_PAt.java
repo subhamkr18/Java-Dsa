5b04086aeed6a82c7ce20077c0fbe5d700e6f023
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-13 23:01:10 +0530

    Aplphabet Pattern Printing

---
+++ Pattern_Printing/ALPHA_PAt.java
@@ -0,0 +1,15 @@
+package Pattern_Printing;
+import java.util.Scanner;
+public class ALPHA_PAt{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n =sc.nextInt();
+
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=n;j++){
+                System.out.print((char)(j+96)+" ");
+            }System.out.println();
+        }
+    }
+}
