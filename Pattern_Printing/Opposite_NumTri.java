ae2f4b8a6741069fb40c5c56825147dfb27afd2d
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-13 23:41:16 +0530

    Opposite Tringle Pattern

---
+++ Pattern_Printing/Opposite_NumTri.java
@@ -0,0 +1,17 @@
+package Pattern_Printing;
+import java.util.Scanner;
+public class Opposite_NumTri {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n= sc.nextInt();
+
+        for(int i=n; i>=1;i--){
+            for(int j=1; j<=i;j++){
+                System.out.print(j+" ");
+            }System.out.println();
+        }
+    }
+}
+
+
