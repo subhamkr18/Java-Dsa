c3e8f364de7da691e29e3b0f467063925e51388b
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-13 23:20:40 +0530

    Opposite Ttinge Pattern

---
+++ Pattern_Printing/Oposte_Tri.java
@@ -0,0 +1,15 @@
+package Pattern_Printing;
+import java.util.Scanner;
+public class Oposte_Tri{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n= sc.nextInt();
+
+        for(int i=n; i>=1;i--){
+            for(int j=1; j<=i;j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+}
