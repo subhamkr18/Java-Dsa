f421be6957ced14233593dce26ff4dc353b12435
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-15 18:30:20 +0530

    Print a rectangle of star inside which there is # filled

---
+++ Pattern_Printing/Hollow_Rec.java
@@ -0,0 +1,29 @@
+package Pattern_Printing;
+
+/*
+* * * * * *
+* # # # # *
+* # # # # *
+* # # # # *
+* * * * * *
+*/
+
+import java.util.Scanner;
+public class Hollow_Rec {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter row: ");
+        int row = sc.nextInt();
+        System.out.print("Enter col: ");
+        int col= sc.nextInt();
+         for(int i=1; i<=row; i++){
+            for(int j=1; j<=col;j++){
+                if(i==1 || i==row || j==1 ||j==col){
+                    System.out.print("*"+" ");
+                }
+                else System.out.print("#"+" ");
+            }
+            System.out.println();
+         }
+    }
+}
