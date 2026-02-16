f7d0aa81e3e71734baee55dbef5bfc22210a2a03
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-16 20:16:35 +0530

    X pattern using loop

---
+++ Pattern_Printing/XPattern.java
@@ -0,0 +1,33 @@
+package Pattern_Printing;
+import java.util.Scanner;
+
+/*
+
+*       *
+  *   *
+    *
+  *   *
+*       *
+
+*/
+public class XPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=n;j++){
+                if(i==j || i+j == n+1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+                
+            }
+            System.out.println();
+        }
+
+    }
+}
