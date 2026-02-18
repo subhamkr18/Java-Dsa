d27d6997f81447a0799ac71bcce94914129027b4
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-19 00:18:48 +0530

    Diamond pattern

---
+++ Pattern_Printing/Diamond.java
@@ -0,0 +1,37 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class Diamond {
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n= sc.nextInt();
+        int nsp =n-1,  nst= 1; // nsp- no of space nst- no of stars
+        for(int i=1; i<=n; i++){
+            
+            for(int j=1;j<=nsp;j++){
+                System.out.print(" "+" ");
+            }
+            for(int k=1; k<=nst; k++){
+                System.out.print("* ");
+            }
+            nsp--; // no of spaces decrement 
+            nst +=2; //no of Star increment with 2
+            System.out.println();
+        }
+        nsp =1; nst= 2*n-3;
+        for(int i=1; i<=n-1; i++){
+            
+            for(int j=1;j<=nsp;j++){
+                System.out.print(" "+" ");
+            }
+            for(int k=1; k<=nst; k++){
+                System.out.print("* ");
+            }
+            nsp++; // no of spaces decrement 
+            nst -=2; //no of Star increment with 2
+            System.out.println();
+    }
+}
+}
