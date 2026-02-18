8f1ab8db04e1ff007d876a129a11542e93c0f85a
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-18 23:54:32 +0530

    Pyramid Pattern

--- Pattern_Printing/Pyramid.java
+++ Pattern_Printing/Pyramid.java
@@ -11,7 +11,7 @@ public class Pyramid {
                 System.out.print(" "+" ");
             }
             for(int k=1; k<=2*i-1; k++){
-                System.out.print("*"+" ");
+                System.out.print("* ");
             }
             System.out.println();
         }
