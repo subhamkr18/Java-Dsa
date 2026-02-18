778c9bddac1f08f3af7e791dfa9784149c242e4a
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-18 23:53:25 +0530

    Horizontal Flipped Tringle code

---
+++ Pattern_Printing/HorizontalyFlippedTringle.java
@@ -0,0 +1,20 @@
+package Pattern_Printing;
+import java.util.Scanner;
+public class HorizontalyFlippedTringle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n =sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print(" "+ " ");
+            }
+            for(int k=1;k<=n-i+1;k++){
+                System.out.print("*"+" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
