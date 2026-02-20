ab760f263c33d29cf4065ede008ff6a662f49918
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-20 23:34:52 +0530

    Swap of two nums

---
+++ Methods/Swap.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a=6;
+        int b=7;
+        int temp = a;
+        System.out.println("a: "+a+"b: "+b);
+        a=b;
+        b= temp;
+        System.out.print("a: "+a+"b: "+b);
+        
+    }
+}
