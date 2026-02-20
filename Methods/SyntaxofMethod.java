5ce8c461c8714fd3e3ebfeaac94a9f7ae1f7fe7e
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-20 22:31:42 +0530

    Method in java

--- Methods/SyntaxofMethod.java
+++ Methods/SyntaxofMethod.java
@@ -2,11 +2,19 @@ package Methods;
 
 public class SyntaxofMethod {
     public static void Greeting(){
-        System.out.print("Subham");
+        System.out.println("Subham");
     }
     public static void main(String[] args) {
+        subham();
         System.out.println("GOod Morning!");
         Greeting();
+        vishal();
         
+    }
+    public static void subham(){
+        System.out.println("Hii Method");
+    }
+    public static void vishal(){
+        System.out.println("I am Vishal");
     }
 }
