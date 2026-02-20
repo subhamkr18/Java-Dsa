42730e6cc92b9537f4781653e5c2cf28f70b1541
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-21 02:08:27 +0530

    Input & output and printing of Array

---
+++ Arrays/OutputInputArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class OutputInputArray {
+    public static void main(String[] args) {
+        int[] arr={5,-8,67,43,-97};
+        // length of array
+        int n = arr.length;
+        //print array using loop
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+        }
+    }
+
