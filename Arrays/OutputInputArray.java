5eff6baec576bf1b091c1a434e1771f83e0d408e
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-22 13:51:08 +0530

    Initialization & taking inputs in Array

--- Arrays/OutputInputArray.java
+++ Arrays/OutputInputArray.java
@@ -1,15 +1,32 @@
 package Arrays;
-
+import java.util.Scanner;
 public class OutputInputArray {
     public static void main(String[] args) {
-        int[] arr={5,-8,67,43,-97};
-        // length of array
-        int n = arr.length;
-        //print array using loop
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
-        }
+        Scanner sc = new Scanner(System.in);
+        // int[] arr={5,-8,67,43,-97};
+        // // length of array
+        // int n = arr.length;
+        // //print array using loop
+        // for(int i=0;i<n;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
         
+
+        int[] Subham = new int[7];// default valur Subham[i]=0
+        
+        // if we not assign vslue to this array its defalut value for each element is 0
+        // for(int i=0; i<7;i++) System.out.print(Subham[i]+" ");
+        
+        //input
+        for(int i=0; i<7;i++){
+            // int x = sc.nextInt();
+            // Subham[i]=x;
+            System.out.println("Enter " +(i+1)+" element: ");
+            Subham[i]=sc.nextInt();
+        }
+        for(int i=0;i<7;i++){
+            System.out.print(Subham[i]+" ");
+        }
         }
     }
 
