89615138594732ec16c9d5dd2c5bba88576fae4d
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-23 22:08:25 +0530

    Taking array input and Print sum of elements

---
+++ Arrays/Input_Sumof_element.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.Scanner;
+public class Input_Sumof_element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n= sc.nextInt();
+        int[] arr =new int[n];
+
+        int sum=0;
+        
+
+        // Array Input
+        for(int i=0; i<n;i++){
+            System.out.println("Enter " +i+ " elements: ");
+            arr[i] = sc.nextInt();
+        }
+
+        // Calculating Sum of elements
+        for(int i=0; i<n;i++){
+            sum += arr[i];
+        }
+        System.out.println("sum of elements of array : "+sum);
+
+    }
+}
