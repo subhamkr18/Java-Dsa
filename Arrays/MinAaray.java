c3a3a45566efe7948b92a06130b373fd33421bf0
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-02-24 21:18:33 +0530

    Finding Min element of an Array

---
+++ Arrays/MinAaray.java
@@ -0,0 +1,30 @@
+package Arrays;
+import java.util.Scanner;
+public class MinAaray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n= sc.nextInt();
+        int[] arr =new int[n];
+
+        int Min=0;
+        
+
+        // Array Input
+        for(int i=0; i<n;i++){
+            System.out.print("Enter " +i+ " elements: ");
+            arr[i] = sc.nextInt();
+        }
+
+        // Finding Min elements of an array
+        for(int i=0; i<n;i++){
+            if(arr[i]<Min){
+                Min=arr[i];
+            }
+        }
+        System.out.println("Product of elements of array : "+Min);
+
+    }
+}
+   
+
