d4e07843e80d2869db0ac1567cd75a55641e4c57
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-03-02 12:44:53 +0530

    Using Methods in array

---
+++ Arrays/PassingArraytoMethods.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class PassingArraytoMethods {
+    public static void change(int[] x){
+        x[2]=99;
+    }
+    public static void main(String[] args) {
+        int[] x={10,20,30,40,50};
+        System.out.println("2nd element before change: "+x[2]);
+        change(x); // change value of 2nd index
+        System.out.print("2nd element After chnge: "+x[2]);
+    }
+}
