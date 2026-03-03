33601afb9a8b733e9be0767de7c96f99cb482f1a
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-03-03 20:47:29 +0530

    minimal change

--- Arrays/PassingArraytoMethods.java
+++ Arrays/PassingArraytoMethods.java
@@ -6,6 +6,7 @@ public class PassingArraytoMethods {
     }
     public static void main(String[] args) {
         int[] x={10,20,30,40,50};
+        System.out.println("Array before  value change: "+x);
         System.out.println("2nd element before change: "+x[2]);
         change(x); // change value of 2nd index
         System.out.print("2nd element After chnge: "+x[2]);
