c4ff910d34fc82ef19c46fcc1ed6077424e375d4
SUBHAM KUMAR <24f3003687@ds.study.iitm.ac.in>
2026-03-05 22:35:06 +0530

    minor change

--- Arrays/MinAaray.java
+++ Arrays/MinAaray.java
@@ -18,6 +18,7 @@ public class MinAaray {
 
         // Finding Min elements of an array
         for(int i=0; i<n;i++){
+            //checking if element is min
             if(arr[i]<Min){
                 Min=arr[i];
             }
