2514a1b823cd02b87cffe3c9069e0361b1ba040e
SUBHAM KUMAR <24f3003687@ds.study.iitm.ac.in>
2026-03-06 22:26:01 +0530

    Clarify arr.length usage with a comment
    Added comment to clarify the purpose of arr.length.

--- Arrays/Print_neg_element_of_Array.java
+++ Arrays/Print_neg_element_of_Array.java
@@ -4,7 +4,7 @@ public class Print_neg_element_of_Array {
     public static void main(String[] args) {
         int arr[] ={-2,-76,98,-65,87};
         // Question print only neg element of array
-        int n= arr.length;
+        int n= arr.length; // arr.length finds the length odf array
         for(int i=0;i<n;i++){
             if(arr[i]<0) System.out.print(arr[i]+" ");
         }
