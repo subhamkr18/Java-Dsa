1a52afd99e20acdba647e6de109d67e56aee3747
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-03-09 22:49:30 +0530

    Code with condition to print month

---
+++ OOP/Month.java
@@ -0,0 +1,54 @@
+package OOP;
+import java.util.Scanner;
+public class Month {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter month number (1-12): ");
+        int monthNumber = scanner.nextInt();
+        String monthName;
+
+        switch (monthNumber) {
+            case 1:
+                monthName = "January";
+                break;
+            case 2:
+                monthName = "February";
+                break;
+            case 3:
+                monthName = "March";
+                break;
+            case 4:
+                monthName = "April";
+                break;
+            case 5:
+                monthName = "May";
+                break;
+            case 6:
+                monthName = "June";
+                break;
+            case 7:
+                monthName = "July";
+                break;
+            case 8:
+                monthName = "August";
+                break;
+            case 9:
+                monthName = "September";
+                break;
+            case 10:
+                monthName = "October";
+                break;
+            case 11:
+                monthName = "November";
+                break;
+            case 12:
+                monthName = "December";
+                break;
+            default:
+                monthName = "Invalid month number";
+        }
+
+        System.out.println("Month: " + monthName);
+        scanner.close();
+    }
+}
