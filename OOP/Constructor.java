3c43c554c3d5007265f258b3344d472869ab05f8
subhamkr18 <24f3003687@ds.study.iitm.ac.in>
2026-03-14 01:10:54 +0530

    Constructor & constructor overloading in java

---
+++ OOP/Constructor.java
@@ -0,0 +1,30 @@
+package OOP;
+
+public class Constructor {
+    public static void main(String[] args) {
+       Student s1= new Student(20);
+       Student s2= new Student(59,21);
+       s1.print();
+       s2.print();
+    }
+}
+
+class Student{
+    int a;
+    int b;
+
+    public Student(int ID){
+        this.a=ID;
+        b=20;
+    }
+
+    public Student(int ID,int Age){
+        this.a=ID;
+        this.b=Age;
+
+    }
+
+    void print(){
+        System.out.println("Student ID: "+a+" age: "+b);
+    }
+}
